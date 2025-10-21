fa35c63e610aa0ae073615d9f346c43a017ce1c7
John Carl Samson <jccsamson@mymail.mapua.edu.ph>
2025-10-21 21:18:52 +0800

    Update Google auth endpoint to use localhost URL
    Changed the Google authentication fetch URL in LoginPage.jsx from a relative path to 'http://localhost:5000/api/auth/google' to ensure requests are sent to the correct backend during development.

--- front-end/Website/react-app/src/LoginPage.jsx
+++ front-end/Website/react-app/src/LoginPage.jsx
@@ -165,7 +165,7 @@ function LoginForm() {
       console.log("📧 Google user info:", decoded);
 
       // Send to backend
-      const response = await fetch("/api/auth/google", {
+      const response = await fetch("http://localhost:5000/api/auth/google", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
