886cfbcc067f552b2ffcddda1ca0952206a7cdc5
Kayle Matthew Calagui <kmmcalagui@mymail.mapua.edu.ph>
2025-10-21 20:42:26 +0800

    added render as web service

--- front-end/Website/react-app/src/LoginPage.jsx
+++ front-end/Website/react-app/src/LoginPage.jsx
@@ -96,7 +96,7 @@ function LoginForm() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
