312d1f9412988550563057d4713060c6b969483f
John Carl Samson <jccsamson@mymail.mapua.edu.ph>
2025-11-17 19:08:22 +0800

    fadawda

--- front-end/Website/react-app/src/DashboardSection.jsx
+++ front-end/Website/react-app/src/DashboardSection.jsx
@@ -34,15 +34,6 @@ const DashboardSection = () => {
   const [retryCount, setRetryCount] = useState(0);
 
   // Fetch user stats and job recommendations
-
-  const getAuthHeaders = () => {
-    const token = localStorage.getItem('token');
-    return {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    };
-  };
-
   useEffect(() => {
     const fetchDashboardData = async () => {
       // Wait for userData to be fully loaded
